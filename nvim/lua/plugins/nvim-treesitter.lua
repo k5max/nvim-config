19f9068b073b0c39495578b194d4b099822b47e3
liwu <aledsanlw@gmail.com>
2023-08-20 05:01:38 +0800

    update: nvim-treesitter auto-install config

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -15,7 +15,7 @@ return {
 
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
+            auto_install = false,
 
             -- List of parsers to ignore installing (for "all")
             ignore_install = {},
