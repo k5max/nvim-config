5c7aafbbcac67ea8bdf8bfd27ea9583076f97b39
k5max <aledsanlw@gmail.com>
2023-08-25 22:14:35 +0800

    update: nvim-cmp config

--- nvim/lua/lsp/lua.lua
+++ nvim/lua/lsp/lua.lua
@@ -2,6 +2,8 @@
 local runtime_path = vim.split(package.path, ';')
 
 local opts = {
+    -- lsp 结合 cmp 自动补全
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
     settings = {
         Lua = {
             runtime = {
--- nvim/lua/lsp/pyright.lua
+++ nvim/lua/lsp/pyright.lua
@@ -1,4 +1,6 @@
 local opts = {
+    -- lsp 结合 cmp 自动补全
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
     settings = {
         python = {
             analysis = {
