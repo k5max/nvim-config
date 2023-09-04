1171c783c8a78d0e8edff3f18b6a1c8f3456d464
k5max <aledsanlw@gmail.com>
2023-09-04 21:51:27 +0800

    update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,30 +2,31 @@
 return {
     {
         "navarasu/onedark.nvim",
-        enabled = true,
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            local colorscheme = "onedark"
-            local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-            if not status_ok then
-                vim.notify("colorscheme " .. colorscheme .. " not found")
-                return
-            end
-        end,
-    },
-    {
-        "sainnhe/sonokai",
         enabled = false,
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            local colorscheme = "sonokai"
-            local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-            if not status_ok then
-                vim.notify("colorscheme " .. colorscheme .. " not found")
-                return
-            end
+            -- 使用 pcall 命令
+            -- local colorscheme = "onedark"
+            -- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+            -- if not status_ok then
+            --     vim.notify("colorscheme " .. colorscheme .. " not found")
+            --     return
+            -- end
+            -- vim.cmd("colorscheme onedark")
+            require('onedark').setup {
+                style = 'dark'
+            }
+            require('onedark').load() -- 跟vim.cmd("colorscheme onedark")效果一样，是两种不同的应用主题的方式
+        end,
+    },
+    {
+        "sainnhe/sonokai",
+        enabled = true,
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            vim.cmd("colorscheme sonokai")
         end,
     },
     {
@@ -34,12 +35,7 @@ return {
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            local colorscheme = "gruvbox"
-            local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-            if not status_ok then
-                vim.notify("colorscheme " .. colorscheme .. " not found")
-                return
-            end
+            vim.cmd("colorscheme gruvbox")
         end,
     },
     {
@@ -48,12 +44,21 @@ return {
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            local colorscheme = "everforest"
-            local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-            if not status_ok then
-                vim.notify("colorscheme " .. colorscheme .. " not found")
-                return
-            end
+            vim.cmd("colorscheme everforest")
+        end,
+    },
+    {
+        "hardhackerlabs/theme-vim",
+        name = "hardhacker",
+        enabled = false,
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.hardhacker_hide_tilde = 1
+            vim.g.hardhacker_keyword_italic = 1
+            -- custom highlights
+            vim.g.hardhacker_custom_highlights = {}
+            vim.cmd("colorscheme hardhacker")
         end,
     },
 }
