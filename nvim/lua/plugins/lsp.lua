387109562d01df9de9a7e5186590eee22c14092e
k5max <aledsanlw@gmail.com>
2023-09-09 01:21:11 +0800

    update lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -126,6 +126,7 @@ return {
             vim.diagnostic.config({
                 virtual_text = true, -- 出错时在行尾显示错误信息
                 signs = false, -- TODO 通过开关设置sign信息，默认关闭
+                underline = true,
                 -- 在输入模式下也更新提示，设置为 true 也许会影响性能
                 update_in_insert = false,
             })
