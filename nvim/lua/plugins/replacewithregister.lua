e561d0f717b10ec1aa6036c9a466a2d26f2ef3bf
liwu <aledsanlw@gmail.com>
2023-09-18 22:18:25 +0800

    update replacewithregister config

--- nvim/lua/plugins/replacewithregister.lua
+++ nvim/lua/plugins/replacewithregister.lua
@@ -2,8 +2,8 @@ return {
     "vim-scripts/ReplaceWithRegister",
     config = function()
         local opt = { noremap = true, silent = true }
-        vim.keymap.set("n", "rs", "<Plug>ReplaceWithRegisterOperator", opt)
-        vim.keymap.set("n", "rss", "<Plug>ReplaceWithRegisterLine", opt)
-        vim.keymap.set("v", "rs", "<Plug>ReplaceWithRegisterVisual", opt)
+        vim.keymap.set("n", "<leader>r", "<Plug>ReplaceWithRegisterOperator", opt)
+        vim.keymap.set("n", "<leader>rr", "<Plug>ReplaceWithRegisterLine", opt)
+        vim.keymap.set("v", "<leader>r", "<Plug>ReplaceWithRegisterVisual", opt)
     end,
 }
