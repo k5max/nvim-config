24cdacb62366100596c0b7ea8acd632fa966d4ee
k5max <aledsanlw@gmail.com>
2024-05-07 23:04:35 +0800

    update: leetcode default closed

--- nvim/lua/plugins/leetcode.lua
+++ nvim/lua/plugins/leetcode.lua
@@ -1,90 +1,90 @@
 return {
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html",
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim", -- telescope 所需
-        "MunifTanjim/nui.nvim",
-
-        -- 可选
-        "nvim-treesitter/nvim-treesitter",
-        "rcarriga/nvim-notify",
-        "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-        arg = "leetcode",
-
-        lang = "java",
-
-        cn = { -- leetcode.cn
-            enabled = true,
-            translator = false,
-            translate_problems = true,
-        },
-
-        storage = {
-            home = vim.fn.stdpath("data") .. "/leetcode",
-            cache = vim.fn.stdpath("cache") .. "/leetcode",
-        },
-
-        plugins = {
-            non_standalone = false,
-        },
-
-        logging = true,
-
-        injector = {},
-
-        cache = {
-            update_interval = 60 * 60 * 24 * 7,
-        },
-
-        console = {
-            open_on_runcode = true,
-
-            dir = "row",
-
-            size = {
-                width = "90%",
-                height = "75%",
-            },
-
-            result = {
-                size = "60%",
-            },
-
-            testcase = {
-                virt_text = true,
-                size = "40%",
-            },
-        },
-
-        description = {
-            position = "left",
-            width = "40%",
-            show_stats = true,
-        },
-
-        hooks = {
-            ["enter"] = {},
-
-            ["question_enter"] = {},
-
-            ["leave"] = {},
-        },
-
-        keys = {
-            toggle = { "q" },
-            confirm = { "<CR>" },
-
-            reset_testcases = "r",
-            use_testcase = "U",
-            focus_testcases = "H",
-            focus_result = "L",
-        },
-
-        theme = {},
-
-        image_support = false,
-    }
+    -- "kawre/leetcode.nvim",
+    -- build = ":TSUpdate html",
+    -- dependencies = {
+    --     "nvim-telescope/telescope.nvim",
+    --     "nvim-lua/plenary.nvim", -- telescope 所需
+    --     "MunifTanjim/nui.nvim",
+    --
+    --     -- 可选
+    --     "nvim-treesitter/nvim-treesitter",
+    --     "rcarriga/nvim-notify",
+    --     "nvim-tree/nvim-web-devicons",
+    -- },
+    -- opts = {
+    --     arg = "leetcode",
+    --
+    --     lang = "java",
+    --
+    --     cn = { -- leetcode.cn
+    --         enabled = true,
+    --         translator = false,
+    --         translate_problems = true,
+    --     },
+    --
+    --     storage = {
+    --         home = vim.fn.stdpath("data") .. "/leetcode",
+    --         cache = vim.fn.stdpath("cache") .. "/leetcode",
+    --     },
+    --
+    --     plugins = {
+    --         non_standalone = false,
+    --     },
+    --
+    --     logging = true,
+    --
+    --     injector = {},
+    --
+    --     cache = {
+    --         update_interval = 60 * 60 * 24 * 7,
+    --     },
+    --
+    --     console = {
+    --         open_on_runcode = true,
+    --
+    --         dir = "row",
+    --
+    --         size = {
+    --             width = "90%",
+    --             height = "75%",
+    --         },
+    --
+    --         result = {
+    --             size = "60%",
+    --         },
+    --
+    --         testcase = {
+    --             virt_text = true,
+    --             size = "40%",
+    --         },
+    --     },
+    --
+    --     description = {
+    --         position = "left",
+    --         width = "40%",
+    --         show_stats = true,
+    --     },
+    --
+    --     hooks = {
+    --         ["enter"] = {},
+    --
+    --         ["question_enter"] = {},
+    --
+    --         ["leave"] = {},
+    --     },
+    --
+    --     keys = {
+    --         toggle = { "q" },
+    --         confirm = { "<CR>" },
+    --
+    --         reset_testcases = "r",
+    --         use_testcase = "U",
+    --         focus_testcases = "H",
+    --         focus_result = "L",
+    --     },
+    --
+    --     theme = {},
+    --
+    --     image_support = false,
+    -- }
 }
