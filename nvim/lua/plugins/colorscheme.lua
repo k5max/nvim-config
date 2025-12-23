d4f9a91427327bca192a5fb3af7b3c5b748b407e
k5max <aledsanlw@gmail.com>
2025-12-24 00:12:22 +0800

    update neovim config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -139,18 +139,4 @@ return {
             vim.cmd("colorscheme everforest")
         end,
     },
-    {
-        "hardhackerlabs/theme-vim",
-        name = "hardhacker",
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.hardhacker_hide_tilde = 1
-            vim.g.hardhacker_keyword_italic = 1
-            -- custom highlights
-            vim.g.hardhacker_custom_highlights = {}
-            vim.cmd("colorscheme hardhacker")
-        end,
-    },
 }
